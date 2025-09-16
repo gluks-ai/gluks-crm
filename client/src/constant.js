e4f7496756a4056d9f1ce1bbdc986749bbe5bdfc
Alexandro <52381045+AlexandroLuis@users.noreply.github.com>
2025-09-16 12:18:12 +0100

    Update constant.js

--- client/src/constant.js
+++ client/src/constant.js
@@ -1,4 +1,4 @@
 export const constant = {
-  baseUrl: "http://127.0.0.1:5001/", 
-  //baseUrl: process.env.REACT_APP_BASE_URL,
+  baseUrl: process.env.REACT_APP_BASE_URL,
+  //baseUrl: "http://127.0.0.1:5001/", 
 };
